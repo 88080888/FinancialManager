ca16ca695fdb77618c512e387bea1535328c195c
Katarzyna <kkazimierczuk@gmail.com>
2022-04-25 11:25:34 +0200

    import all required elements from html to js

---
+++ script.js
@@ -0,0 +1,14 @@
+const incomeArea = document.querySelector('.income-area');
+const expensesArea = document.querySelector('.exoenses-area');
+const avaliableMoney = document.querySelector('.avaliable-money');
+
+const addTransactionPanel = document.querySelector('.add-transaction-panel');
+const nameInput = document.querySelector('#name');
+const amountInput = document.querySelector('#amount');
+const categorySelect = document.querySelector('#category');
+
+const addTransactionBtn = document.querySelector('.add-transaction');
+const deleteBtn = document.querySelector('.delete');
+const deleteAllBtn = document.querySelector('.delete-all');
+const saveBtn = document.querySelector('.save');
+const cancelBtn = document.querySelector('.cancel');
