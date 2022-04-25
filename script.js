1b09a3a329801e593b00768687b4617d130c8c03
Katarzyna <kkazimierczuk@gmail.com>
2022-04-25 21:35:50 +0200

    create createNewTransaction function to add new transactions and assigned icons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const incomeArea = document.querySelector('.income-area');
-const expensesArea = document.querySelector('.exoenses-area');
+const expensesArea = document.querySelector('.expenses-area');
 const avaliableMoney = document.querySelector('.avaliable-money');
 
 const addTransactionPanel = document.querySelector('.add-transaction-panel');
@@ -30,7 +30,7 @@ const closeAddTransactionPanel= () => {
 
 const checkForm = () => {
   if (nameInput.value !== '' && amountInput.value !== '' && categorySelect.value !== 'none') {
-    console.log('ok');
+    createNewTransaction();
   } else {
     alert('No field can be empty!');
   }
@@ -42,7 +42,50 @@ const clearForm = () => {
   categorySelect.selectedIndex = 0;
 }
 
+const createNewTransaction = () => {
+  const newTransaction = document.createElement('div');
+  newTransaction.classList.add('transaction');
+  newTransaction.setAttribute('id', ID);
+
+  checkCategory(selectedCategory);
+
+  newTransaction.innerHTML = `
+    <p class="transaction-name shopping-title">${categoryIcon} ${nameInput.value}</p>
+    <p class="transaction-amount"> ${amountInput.value}zł <button class="delete"><i class="fa-solid fa-x"></i></button></p>
+  `
+  amountInput.value > 0 ? incomeArea.appendChild(newTransaction) && newTransaction.classList.add('income') : expensesArea.appendChild(newTransaction) && newTransaction.classList.add('expense');
+
+  moneyArr.push(parseFloat(amountInput.value));
+
+  closeAddTransactionPanel();
+  ID++;
+  clearForm();
+}
+
+const selectCategory = () => {
+  selectedCategory = categorySelect.options[categorySelect.selectedIndex].text;
+}
+
+const checkCategory = transaction => {
+  switch (transaction) {
+    case '[ + ] Salary':
+      categoryIcon = '<i class="fa-solid fa-money-bill-1"></i>';
+      break;
+    case '[ + ] Benefit':
+      categoryIcon = '<i class="fa-solid fa-square-plus"></i>';
+      break;
+    case '[ - ] Shopping':
+      categoryIcon = '<i class="fa-solid fa-bag-shopping"></i>';
+      break;
+    case '[ - ] Car':
+      categoryIcon = '<i class="fa-solid fa-car"></i>';
+      break;
+    case '[ - ] Health&Beauty':
+      categoryIcon = '<i class="fa-solid fa-heart-pulse"></i>';
+      break;   
+  }
+}
+
 addTransactionBtn.addEventListener('click',showAddTransactionPanel);
 cancelBtn.addEventListener('click',closeAddTransactionPanel);
 saveBtn.addEventListener('click',checkForm);
-
