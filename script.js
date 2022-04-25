6794ee08a6c9c97fd6a3337ace81706b9f2920fb
Katarzyna <kkazimierczuk@gmail.com>
2022-04-25 11:55:27 +0200

    add functions to show and close AddTransactionPanel

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ const deleteBtn = document.querySelector('.delete');
 const deleteAllBtn = document.querySelector('.delete-all');
 const saveBtn = document.querySelector('.save');
 const cancelBtn = document.querySelector('.cancel');
+
+let root = document.documentElement;
+let ID = 0;
+let categoryIcon;
+let selectedCategory;
+let moneyArr = [0];
+
+const showAddTransactionPanel= () => {
+  addTransactionPanel.style.display = 'flex';
+}
+
+const closeAddTransactionPanel= () => {
+  addTransactionPanel.style.display = 'none';
+}
+
+addTransactionBtn.addEventListener('click',showAddTransactionPanel);
+
+cancelBtn.addEventListener('click',closeAddTransactionPanel);
